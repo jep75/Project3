5178961658bf1a94e00088f8b5adec9bfc03abae
John Phillips <johnphillips@johns-mbp>
2018-02-26 15:28:37 -0500

    Initial Commit

---
+++ phillips_project2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  phillips_project2
+//
+//  Created by John Phillips on 2/26/18.
+//  Copyright © 2018 John Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
---
+++ phillips_project2Tests/phillips_project2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  phillips_project2Tests.swift
+//  phillips_project2Tests
+//
+//  Created by John Phillips on 2/26/18.
+//  Copyright © 2018 John Phillips. All rights reserved.
+//
+
+import XCTest
+@testable import phillips_project2
+
+class phillips_project2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
